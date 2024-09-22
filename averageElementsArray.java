a3d32682b573488f035d813054445893d16cc596
rajnish3013-ux <rajnish3013@gmail.com>
2024-09-22 17:15:32 +0530

    Array

---
+++ averageElementsArray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class averageElementsArray {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        int[] arr = {1,2,3,4,5,6,7};
+
+        for(int i=0; i<=arr.length-1; i++){
+
+            sum = sum+arr[i];
+        }
+
+        int average = sum/arr.length;
+
+        
+        System.out.println("Sum of elements of given array: "+sum);
+        System.out.println("Average of elements of given array: "+average);
+        System.out.print("Length of given array: "+arr.length);
+        
+    }
+    
+}
