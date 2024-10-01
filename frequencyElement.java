b902abe3fe559e27a4886a3f8ec5019b7499c892
rajnish3013-ux <rajnish3013@gmail.com>
2024-10-01 12:02:16 +0530

    java

---
+++ frequencyElement.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class frequencyElement {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10,5,10,15,10,5};
+        
+
+        boolean visited[] = new boolean[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+
+            if(visited[i]==true)
+
+                continue;
+
+            
+            
+            
+                int count = 1;
+                
+                for(int j=i+1; j<arr.length; j++){
+
+                if(arr[i]==arr[j]){
+
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.print(arr[i]+ " " +count);
+
+        }
+
+        
+    }
+}
+
+
+
